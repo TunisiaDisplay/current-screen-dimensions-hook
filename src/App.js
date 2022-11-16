22463031fbd8c11a30379c41a0fae922e857bc71
TunisiaDisplay <israr4dev@gmail.com>
2022-11-16 20:32:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import useScreenDimensions from './hooks/useScreenDimentions';
+
 import './App.css';
 
 function App() {
+  const dimensions = useScreenDimensions();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        your current browser window size is 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{dimensions.width} / {dimensions.height}</p>
+        
       </header>
     </div>
   );
